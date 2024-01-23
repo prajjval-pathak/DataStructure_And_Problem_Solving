f59977367ab0cab00edad7785c5bc9551807c41c
Prajjval Pathak <prajjvalpathak@gmail.com>
2024-01-23 19:42:24 +0530

    Breadth First Traversal done

--- 08-binary-trees-graphs/14-graph-depth-first-traversal/graph-depth-first.js
+++ 08-binary-trees-graphs/14-graph-depth-first-traversal/graph-depth-first.js
@@ -1,5 +1,23 @@
 const Stack = require('./stack');
 
-function depthFirstTraversal() {}
+function depthFirstTraversal(graph,start) {
+  if (!graph.adjacencyList[start]) {
+    return [];
+  }
+  const res=[]
+  const stack=[]
+  const visited={}
+  stack.push(start)
+  visited[start]=true
+  while(stack.length>0){
+    const currentVertex=stack.pop()
+    res.push(currentVertex)
+    graph.adjacencyList[currentVertex].forEach((item)=>{if(!visited[item]){
+        stack.push(item)
+        visited[item]=true
+  }})
+  }
+  return res
+}
 
 module.exports = depthFirstTraversal;
